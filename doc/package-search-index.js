e4598c47b505a4e8cb2750fa6cdabb37c4957c6e
boaz tene <boaztene2005@gmail.com>
2021-03-27 15:16:53 +0300

    Added the SQLITE feature

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"DataBase.Table"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"DataBase"},{"l":"DataBase.Table"}]
